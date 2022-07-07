9392ccf6fc95e5c45eba5e5c2c03b20cf06186ed
chenxiaopei <750627595@qq.com>
2022-07-07 19:21:02 +0800

    init next project

---
+++ nextjs/pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next'
+import styles from '../assets/styles/Home.module.css'
+import Link from "next/link";
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+        <Link href={'/about'}>
+            跳到关于
+        </Link>
+        <div>
+            <Link href={'/gallery'}>
+                跳到相册
+            </Link>
+        </div>
+    </div>
+  )
+}
+
+export default Home
