cdd57de17754ff250e05ab7444aa2ca2b34b65ef
chenxiaopei <750627595@qq.com>
2022-07-12 17:13:56 +0800

    feat:stripe gradient

---
+++ nextjs/components/about/StripeGradient/readme.md
@@ -0,0 +1,1 @@
+code in this folder copy form https://github.com/jazeved0/jazevedo.me
--- nextjs/pages/about.tsx
+++ nextjs/pages/about.tsx
@@ -1,8 +1,7 @@
 import type { NextPage } from 'next'
 import styles from '../assets/styles/Home.module.css'
 import dynamic from 'next/dynamic'
-
-const DynamicHeader = dynamic(() => import('../components/about/FoldingGradient'), {
+const DynamicStripe = dynamic(() => import('../components/about/StripeGradient'), {
   ssr: false,
 })
 const About: NextPage = () => {
@@ -12,7 +11,9 @@ const About: NextPage = () => {
   // todo 开源作品模块参考RocketAir | Design + Strategy,往下滑动滚动的块一个个上来
   return (
     <div className={styles.container}>
-      <DynamicHeader />
+      <DynamicStripe
+        style={{width:'100vw',height:'100vh'}}
+        colors={['#ef008f','#6ec3f4','#7038ff','#ffba27']} />
     </div>
   )
 }
